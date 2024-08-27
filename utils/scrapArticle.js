0d06a46a3adba0b7b38a3193168e1c04298c44a9
Aamish26622 <aamishirfan2662@gmail.com>
2024-08-27 11:16:29 +0500

    scraping fixes and adding of default image

--- utils/scrapArticle.js
+++ utils/scrapArticle.js
@@ -7,9 +7,17 @@ async function scrapeArticle(articleLink) {
     await page.goto(articleLink, { waitUntil: 'networkidle2' });
 
     const data = await page.evaluate(() => {
-        const title = document.querySelector('h1')?.innerText;
-        const excerpt = document.querySelector('.brief-excerpt')?.innerText || document.querySelector('.bigger-p')?.innerText;
-        const imageUrl = document.querySelector('figure img')?.src;
+        // Try to get the title from h1; if not available, get it from h2 inside the parent div
+        let title = document.querySelector('h1')?.innerText || document.querySelector('#cover-news-left h2')?.innerText;
+        const excerpt = document.querySelector('.brief-excerpt')?.innerText || document.querySelector('#cover-news-left .bigger-p')?.innerText;
+
+        let imageUrl = window.getComputedStyle(document.querySelector('#cover-news-content'))?.backgroundImage;
+
+        if (imageUrl && imageUrl !== 'none') {
+            imageUrl = imageUrl.slice(5, -2);
+        } else {
+            imageUrl = document.querySelector('figure img')?.src;
+        }
 
         return {
             title,
