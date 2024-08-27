9a46ad84c287d8be5295f6fa8928b92d9a860979
Aamish26622 <aamishirfan2662@gmail.com>
2024-08-27 13:30:38 +0500

    puppeter server issue fix

--- utils/scrapArticle.js
+++ utils/scrapArticle.js
@@ -1,13 +1,15 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeArticle(articleLink) {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
 
     await page.goto(articleLink, { waitUntil: 'networkidle2' });
 
     const data = await page.evaluate(() => {
-        // Try to get the title from h1; if not available, get it from h2 inside the parent div
         let title = document.querySelector('h1')?.innerText || document.querySelector('#cover-news-left h2')?.innerText;
         const excerpt = document.querySelector('.brief-excerpt')?.innerText || document.querySelector('#cover-news-left .bigger-p')?.innerText;
 
