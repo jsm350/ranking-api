e1a9708ee21f8d443dde8fbfd83441c964c4e1f2
Aamish26622 <aamishirfan2662@gmail.com>
2024-12-31 22:06:23 +0500

    route fix

--- routes/distributorRoutes.js
+++ routes/distributorRoutes.js
@@ -5,7 +5,7 @@ const {authenticateToken} = require("../middlewares/authMiddleware");
 
 router.post('/', distributorController.store);
 router.get('/', distributorController.index);
-router.get('/:distributorSlug', authenticateToken, distributorController.show);
+router.get('/:distributorSlug', distributorController.show);
 router.delete('/:distributorId', authenticateToken, distributorController.destroy);
 
 module.exports = router;
